2308ceb481c49f2239b3e814cf10321a180613f9
Viorel Stirbu <viorels@gmail.com>
2016-04-21 12:06:48 +0800

    isomorphic rendering almost working (server browserify vs client babel)

--- server/main.js
+++ server/main.js
@@ -4,10 +4,21 @@ var app = new express();
 
 var parser = require('body-parser');
 
+var React = require('react');
+var ReactDOMServer = require('react-dom/server');
+var GroceryItem = require('./models/GroceryItem.js');
+
+require('babel-register');
 require('./database.js');
 
 app.get('/', function(req, res) {
-    res.render('./../app/index.ejs', {});
+    var application = React.createFactory(require('./../app/components/GroceryItemList.jsx'));
+    GroceryItem.find(function (error, doc) {
+        var generated = ReactDOMServer.renderToString(application({
+           items: doc
+        }));
+        res.render('./../app/index.ejs', {reactOutput: generated});
+    });
 })
 .use(express.static(__dirname + '/../.tmp'))
 .listen(7777);
